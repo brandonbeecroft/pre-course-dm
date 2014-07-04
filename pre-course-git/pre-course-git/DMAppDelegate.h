c811a2f64cd3b961594044edd426f332064bca17
Brandon Beecroft <brandonbeecroft@Brandons-MacBook-Pro.local>
2014-07-04 13:07:51 -0600

    first commit

---
+++ pre-course-git/pre-course-git/DMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DMAppDelegate.h
+//  pre-course-git
+//
+//  Created by Brandon Beecroft on 7/4/14.
+//  Copyright (c) 2014 Brandon Beecroft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ pre-course-git/pre-course-git/DMViewController.h
@@ -0,0 +1,13 @@
+//
+//  DMViewController.h
+//  pre-course-git
+//
+//  Created by Brandon Beecroft on 7/4/14.
+//  Copyright (c) 2014 Brandon Beecroft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMViewController : UIViewController
+
+@end
